e80bffce0405e3beab704172e83b5d456943de77
133397 <dadagfyau@fshfhf>
2024-01-26 12:19:26 +0100

    button gemaakt

--- app/src/main/java/com/example/groeiclicker/Main_Game.kt
+++ app/src/main/java/com/example/groeiclicker/Main_Game.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +23,7 @@ class Main_Game : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    var counter = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,9 +36,17 @@ class Main_Game : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main__game, container, false)
+        val view =  inflater.inflate(R.layout.fragment_main__game, container, false)
+        view.findViewById<ImageButton>(R.id.imageButtonclickhere).setOnClickListener {
+
+            counter = counter + 0.1
+view.findViewById<TextView>(R.id.textcountermain).text = counter.toString()
+
+
+        }
+        return view
     }
+
 
     companion object {
         /**
