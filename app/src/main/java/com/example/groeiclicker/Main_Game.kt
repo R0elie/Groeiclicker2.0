d21fe97933be2e387adafb43831ac6c3d1314cc2
133610 <ygewryugw@uifefie>
2024-03-08 11:51:50 +0100

    knop

--- app/src/main/java/com/example/groeiclicker/Main_Game.kt
+++ app/src/main/java/com/example/groeiclicker/Main_Game.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
@@ -132,7 +133,7 @@ class Main_Game : Fragment() {
             view.findViewById<ImageButton>(R.id.SettingsButtonMainGame).setOnClickListener {
                 Navigation.findNavController(view).navigate(R.id.Settings)
 
-
+                }
 
             }
 
--- app/src/main/java/com/example/groeiclicker/groei_store.kt
+++ app/src/main/java/com/example/groeiclicker/groei_store.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.navigation.Navigation
 
@@ -43,6 +44,11 @@ class groei_store : Fragment() {
 
         view.findViewById<ImageButton>(R.id.settings_button_store).setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.Settings)
+        }
+        view.findViewById<Button>(R.id.melk_button).setOnClickListener {
+            Navigation.findNavController(view).navigateUp()
+
+
         }
 
 
